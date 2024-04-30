d45e9a357498e5d9c0cc497b6a1a4bf6ddb3dad7
Ammar1122s <ammaraamir33@gmail.com>
2024-04-30 22:46:22 +0500

    navbar items

---
+++ components/NavItems.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useState } from "react"
+
+
+const NavItems = () =>{
+    const [activeIndex, setActiveIndex] = useState < null | number  >()
+    return(
+        <div className="flex gap-4 "></div>
+
+    )
+}
+
+export default NavItems
---
+++ config/index.ts
@@ -0,0 +1,14 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label:"UI Kits",
+        value:"ui_kits" as const,
+        featured:[
+            {
+            name: "Editor Packs",
+            image: "/nav/ui-kits/mixed.jpg",
+            herf:"#"
+            },
+            
+        ]
+    }
+]
